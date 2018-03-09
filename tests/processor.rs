323aed36b16c79592a8bdb6c4a426dcd2e1d3b6c
Guilherme Diego <guidiego.expgames@gmail.com>
2018-03-09 18:53:51 -0300

    Add: Travis File

--- tests/processor.rs
+++ tests/processor.rs
@@ -4,7 +4,7 @@ use appa::processor;
 
 #[test]
 fn should_processor_work_for_python_task() {
-let cmd = processor::Command{
+    let cmd = processor::Command{
         command: String::from("python"),
         path: String::from("tests/mocks/tasks/task.py"),
     };
